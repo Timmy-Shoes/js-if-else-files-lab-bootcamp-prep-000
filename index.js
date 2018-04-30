6527851320497f302c6ba62e0d8d540545e95bb0
schoonmt@gmail.com <schoonmt@gmail.com>
2018-04-30 09:04:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var name = "Susan"
-var height = "74"
+var height = "70"
  
 // Don't worry about this
 module.exports = { name, height
 }
-if (height === "74") {
+if (height === "70") {
   console.log("The height is correct")
 } else {
-  console.log("Expected the height to be 74")
+  console.log("Expected the height to be 70")
 }
