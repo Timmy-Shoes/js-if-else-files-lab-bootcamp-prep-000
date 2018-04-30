84e6b8e336b8baf0e994e8d63176cdbe8f1a5e6e
schoonmt@gmail.com <schoonmt@gmail.com>
2018-04-30 09:09:15 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -1,8 +1,8 @@
 var index = require("./index.js")
 if (index.height === 70) {
-    console.log("The height is correct")
+    console.log("The name is correct")
 }
 
 if (index.name === "Susan") {
-    console.log("The name is correct")
+    console.log("The height is correct")
 }
